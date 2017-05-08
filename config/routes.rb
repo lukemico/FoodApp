8c5bc9543f23574c059ccbb1dd245b5ec35f6450
lukemico <lukemico@me.com>
2017-05-08 20:22:06 +1000

    Project 2 added

--- config/routes.rb
+++ config/routes.rb
@@ -7,37 +7,20 @@ resources :venue
 get "/venue/:id/new" => 'venues#new'
 get "/venue/:id/update" => 'venues#update'
 get "/venue/:id/delete" => 'venues#delete'
+
 resources :blog
 get "/blog/:id/new" => 'comments#new'
 get "/blog/:id/update" => 'comments#update'
 get "/blog/:id/delete" => 'comments#delete'
+
 resources :comments
 resources :users
 # resources :login
+
 get '/login' => 'login#new'
 post '/login' => 'login#create'
 delete '/logout' => 'login#delete'
 
 get "/sass.html" => 'home#index'
 
-
-
 end
-
-# get 'login/new'
-
-# get 'login/show'
-
-# get 'login/index'
-#
-# get 'login/update'
-
-# get 'blog/index'
-#
-# get 'blog/show'
-#
-# get 'blog/update'
-#
-# root to: 'pages#index'
-#
-# get 'home/index'
